15bf7abd196bcceb91444cc3ba9e33b603bdb1cb
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2021-12-02 04:25:09 -0300

    dividir polinomios

--- src/main.f90
+++ src/main.f90
@@ -6,6 +6,7 @@ program main
 
    integer                      :: grado, i
    real (kind = 8), allocatable :: raices(:), coeficientes(:), raices_estimadas(:)
+   real (kind=8), dimension(3)  :: factor_cuad
 
    write(*,*) 'Ingrese el grado del polinomio:'
    read (*,*)  grado
@@ -35,5 +36,11 @@ program main
    write(*,*) 'Las raices calculadas con 10 iteraciones de qd, y mejoradas con 5 iteraciones del metodo de newton son:'
    call mostrar_vector(calcular_raices_qd_nwt(grado, coeficientes, 10, 5), grado, .true.)
 
+   write(*,*) 'El polinomio dividido la primer raiz ingresada es:'
+   call mostrar_polinomio(grado-1, dividir_por_factor_lineal(grado, coeficientes, raices(1)))
+
+   factor_cuad = generar_polinomio(2,  raices(1:2))
+   write(*,*) 'El polinomio dividido las primeras 2 raices ingresadas es:'
+   call mostrar_polinomio(grado-2, dividir_por_factor_cuadratico(grado, coeficientes,factor_cuad(2),factor_cuad(3)))
    read (*,*)
 end program main
--- src/util.f90
+++ src/util.f90
@@ -8,6 +8,7 @@ subroutine mostrar_vector(vec, n, mismaLinea)
    integer                       :: n, i
    real (kind = 8), dimension(n) :: vec
    logical                       :: mismaLinea !si es verdadero todos los elementos del vector, de ser posible se mostrar en una misma linea, sino se mostraran uno por linea
+
    do i=1, n
       if (mismaLinea) then 
          write (*, FMT_NUMEROS, advance = "no") vec(i)
@@ -16,23 +17,26 @@ subroutine mostrar_vector(vec, n, mismaLinea)
       end if
    end do
    write(*,*) 
+
 end subroutine
 
 subroutine mostrar_matriz(mat, n, m)
    implicit none
-
    integer                         :: n, m, i, j
    real (kind = 8), dimension(n,m) :: mat
+
    do i=1, n
       call mostrar_vector(mat(i,:), m, .true.)
       write(*,*) 
    end do
+
 end subroutine mostrar_matriz
 
 subroutine mostrar_polinomio(grado, coeficientes)
    implicit none
    integer                             :: grado, i
    real (kind = 8), dimension(grado+1) :: coeficientes
+
    do i=1, grado+1
       if(coeficientes(i) .GE. 0) then
          write (*,'(A,F0.4,A,I0)', advance = "no") '+ ', coeficientes(i),'x^', grado+1-i
@@ -41,6 +45,7 @@ subroutine mostrar_polinomio(grado, coeficientes)
       endif
    end do
    write(*,*)
+
 end subroutine
 
 
@@ -49,6 +54,7 @@ function calcular_derivada(grado, coeficientes)
    integer                             :: grado, i
    real (kind = 8), dimension(grado+1) :: coeficientes
    real (kind = 8), dimension(grado)   :: calcular_derivada
+
    do i=1, grado
       calcular_derivada(i) = coeficientes(i)*(grado-i +1)
    end do
@@ -60,10 +66,46 @@ function evaluar_polinomio(grado, coeficientes, x)
    integer                             :: grado, i
    real (kind = 8), dimension(grado+1) :: coeficientes
    real (kind = 8)                     :: x, valor, evaluar_polinomio
+
    do i=1, grado+1
       valor = valor + coeficientes(i)*(x**(grado+1-i)) 
    end do 
    evaluar_polinomio = valor;
+
 end function evaluar_polinomio
+
+! devuelve el resultado de dividir un polinomio por (x-x0), siendo x0 una de las raices del polinomio
+! realiza la division por rufini
+function dividir_por_factor_lineal(grado, coeficientes, x0)
+   implicit none
+   integer                           :: grado, i
+   real (kind=8)                     :: x0, aux
+   real (kind=8), dimension(grado)   :: dividir_por_factor_lineal
+   real (kind=8), dimension(grado+1) :: coeficientes
+
+   aux = 0
+   do i=1, grado+1
+      dividir_por_factor_lineal(i) = coeficientes(i) + aux
+      aux = (coeficientes(i) + aux) * x0
+   end do
+
+end function dividir_por_factor_lineal
+
+! devuelve el resultado de dividir un polinomio por (x^2 + bx + c)
+function dividir_por_factor_cuadratico(grado, coeficientes, b, c)
+   implicit none
+   integer                           :: grado, grado_act, i
+   real (kind=8)                     :: b, c, cp
+   real (kind=8), dimension(grado-1) :: dividir_por_factor_cuadratico
+   real (kind=8), dimension(grado+1) :: coeficientes, aux
+
+   aux = coeficientes
+   do i=1, grado-1
+      dividir_por_factor_cuadratico(i) = aux(i)
+      aux(i+1) = aux(i+1) - b*aux(i)
+      aux(i+2) = aux(i+2) - c*aux(i)  
+   end do
+
+end function dividir_por_factor_cuadratico
 
 end module util
